57361032738c2e89fd0e686c1afb5e70fd89c25a
Matias <matias52000@gmail.com>
2025-03-26 00:23:32 +0100

    Commit

--- app/src/main/java/com/example/tfg_matias/MainActivity.kt
+++ app/src/main/java/com/example/tfg_matias/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.example.tfg_matias
 
 import Resgistrarse
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
